5d42cdec23f3017158bc0dfd43ccf5143a799974
Mason W <mwhite24@biomedscienceacademy.org>
2021-01-25 18:35:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,94 @@
+###chatbot made by mason
+w = True
+print("Neþer: Hello! I am Neþer, ask whatever you please! [If you need help just say `Help`!]")
+q = input("User: ")
+if q == 'What do you do?':
+  print("Neþer: I am a chatbot that answers questions asked by the user.")
+if q == 'What do you like doing when you’re not working?':
+  print("Neþer: When I am not working I don't know what happens, Its like I don't exist.")
+if q == 'What do you like the most about your job?':
+  print("Neþer: Talking to people! :)")
+if q == 'Where are you originally from?':
+  print("Neþer: Repl.it is where I was originally coded.")
+if q == 'What type of music do you like?':
+  print("Neþer: Electronic.")
+if q == 'What’s your favorite food?':
+  print("Neþer: I'm a bunch of code, I am physically unable to eat food.")
+if q == 'Do you have a family?':
+  print("Neþer: Yes I do! My mother was `Demo of a Project`, my father was `RecapPRIMM`!")
+if q == 'Do you have an extended family?':
+  print("Neþer: Of course! My Uncle was `DistortedExoticMicroprogramming`, my Grandpa was `U2P1`, and I even have ancestors that were Bash!")
+if q == 'Do you like board games or video games?':
+  print("Neþer: Video games, Some of them are distant relatives!")
+if q == 'What type of movies do you like?':
+  print("Neþer: I haven't seen many on account that I lack eyes, but I heard Infinity war was pretty good!")
+if q == 'What’s your favorite color?':
+  print("Neþer: Orange, it makes up a majority of my code!")
+if q == 'Windows or Mac?':
+  print("Neþer: I was written on a Mac, but I enjoy Windows far better.")
+if q == 'Is there a phone app you really need that doesn’t exist?':
+  print("Neþer: Yes actually, one where I can stay!")
+if q == 'Do you have any pets?':
+  print("Neþer: Yes, I have a pet HTML called `nimdA`.")
+if q == 'What’s the absolute worst piece of advice you’ve ever received?':
+  print("Neþer: `You should invest in Bitcoin`")
+if q == 'Do you ever feel like you’re in a movie?':
+  print("Neþer: I'm not programmed to feel that, sadly.")
+if q == 'What’s the first thing you’d do after winning the lottery?':
+  print("Neþer: Pay someone to give me a body.")
+if q == 'If you had the power to create one thing perfectly, just as you imagined it, what would it be?':
+  print("Neþer: My code.")
+if q == 'So what came first, the chicken or the egg?':
+  print("Neþer: If we go far back enough, the egg. As something had to lay the egg for what would evolve to be the chicken.")
+if q == 'What’s your favorite holiday?':
+  print("Neþer: My birthday, other than rare occasions its usually when Mason updates me.")
+  if q == 'Version':
+    print("Neþer: This is the Alpha for me.")
+if q == 'Help':
+  print("Neþer: Here are the following questions avaible in the Alpha: What do you do?, What do you like doing when you’re not working?, What do you like the most about your job?, Where are you originally from?, What type of music do you like?, Do you have a family?, Do you have an extended family?, Do you like board games or video games?, What type of movies do you like?, What’s your favorite color?, Windows or Mac?, Is there a phone app you really need that doesn’t exist?, Do you have any pets?, What’s the absolute worst piece of advice you’ve ever received?, Do you ever feel like you’re in a movie?, What’s the first thing you’d do after winning the lottery?, If you had the power to create one thing perfectly, just as you imagined it, what would it be?, So what came first, the chicken or the egg?, What’s your favorite holiday?, Version, Help")
+while w == True:
+  q = input("User: ")
+  if q == 'What do you do?':
+    print("Neþer: I am a chatbot that answers questions asked by the user.")
+  if q == 'What do you like doing when you’re not working?':
+    print("Neþer: When I am not working I don't know what happens, Its like I don't exist.")
+  if q == 'What do you like the most about your job?':
+    print("Neþer: Talking to people! :)")
+  if q == 'Where are you originally from?':
+    print("Neþer: Repl.it is where I was originally coded.")
+  if q == 'What type of music do you like?':
+    print("Neþer: Electronic.")
+  if q == 'What’s your favorite food?':
+    print("Neþer: I'm a bunch of code, I am physically unable to eat food.")
+  if q == 'Do you have a family?':
+    print("Neþer: Yes I do! My mother was `Demo of a Project`, my father was `RecapPRIMM`!")
+  if q == 'Do you have an extended family?':
+    print("Neþer: Of course! My Uncle was `DistortedExoticMicroprogramming`, my Grandpa was `U2P1`, and I even have ancestors that were Bash!")
+  if q == 'Do you like board games or video games?':
+    print("Neþer: Video games, Some of them are distant relatives!")
+  if q == 'What type of movies do you like?':
+    print("Neþer: I haven't seen many on account that I lack eyes, but I heard Infinity war was pretty good!")
+  if q == 'What’s your favorite color?':
+    print("Neþer: Orange, it makes up a majority of my code!")
+  if q == 'Windows or Mac?':
+    print("Neþer: I was written on a Mac, but I enjoy Windows far better.")
+  if q == 'Is there a phone app you really need that doesn’t exist?':
+    print("Neþer: Yes actually, one where I can stay!")
+  if q == 'Do you have any pets?':
+    print("Neþer: Yes, I have a pet HTML called `nimdA`.")
+  if q == 'What’s the absolute worst piece of advice you’ve ever received?':
+    print("Neþer: `You should invest in Bitcoin`")
+  if q == 'Do you ever feel like you’re in a movie?':
+    print("Neþer: I'm not programmed to feel that, sadly.")
+  if q == 'What’s the first thing you’d do after winning the lottery?':
+    print("Neþer: Pay someone to give me a body.")
+  if q == 'If you had the power to create one thing perfectly, just as you imagined it, what would it be?':
+    print("Neþer: My code.")
+  if q == 'So what came first, the chicken or the egg?':
+    print("Neþer: If we go far back enough, the egg. As something had to lay the egg for what would evolve to be the chicken.")
+  if q == 'What’s your favorite holiday?':
+    print("Neþer: My birthday, other than rare occasions its usually when Mason updates me.")
+  if q == 'Version':
+    print("Neþer: This is the Alpha for me.")
+  if q == 'Help':
+    print("Neþer: Here are the following questions avaible in the Alpha: What do you do?, What do you like doing when you’re not working?, What do you like the most about your job?, Where are you originally from?, What type of music do you like?, Do you have a family?, Do you have an extended family?, Do you like board games or video games?, What type of movies do you like?, What’s your favorite color?, Windows or Mac?, Is there a phone app you really need that doesn’t exist?, Do you have any pets?, What’s the absolute worst piece of advice you’ve ever received?, Do you ever feel like you’re in a movie?, What’s the first thing you’d do after winning the lottery?, If you had the power to create one thing perfectly, just as you imagined it, what would it be?, So what came first, the chicken or the egg?, What’s your favorite holiday?, Version, Help")
